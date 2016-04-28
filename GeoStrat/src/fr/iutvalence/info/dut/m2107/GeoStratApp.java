5395a3ac13837d8714b6ef4e7dd5fa32fbd88967
fluchaiv <fluchaiv@PC-B123-12.iut-valence.fr>
2016-04-28 08:34:31 +0200

    Un test de ma part pour voir si tout fonctionne bien.

--- GeoStrat/src/fr/iutvalence/info/dut/m2107/GeoStratApp.java
+++ GeoStrat/src/fr/iutvalence/info/dut/m2107/GeoStratApp.java
@@ -1,8 +1,15 @@
 package fr.iutvalence.info.dut.m2107;
 
-
+/**
+ * Principal application for our project
+ * @author Team
+ */
 public class GeoStratApp 
 {
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args)
 		{
 			
