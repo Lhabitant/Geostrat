9c6ecae658a3c2caba53ae0d00a0227f0d114f00
lecomtel <lecomtel@PC-B103-02.iut-valence.fr>
2016-05-11 13:48:18 +0200

    //!\\ it's don't work, why ? because i test a animation and it doesn"t work, but we have a new button on the screen \o/

--- GeoStrat/src/fr/iutvalence/info/dut/m2107/InterfaceUtilisateur.java
+++ GeoStrat/src/fr/iutvalence/info/dut/m2107/InterfaceUtilisateur.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import java.awt.Color; 
 import java.awt.Graphics;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -88,14 +89,137 @@ public class InterfaceUtilisateur extends JFrame {
 	    // prevent Jframe that Jpanel will be content panel
 	    this.setContentPane(pan);               
 	    this.setVisible(true);
+	    
+	    // create a new button
+	    JButton bouton = new JButton();
+	    bouton.setText("Mon bouton");
+	    
+	    //add the button on the screen
+	    pan.add(bouton);
+	    
+	    //the animation
+	    go();
 	}
 
-	  public void paintComponent(Graphics g){
-
-		    System.out.println("I'm running !"); 
-
-		    g.fillOval(20, 20, 75, 75);
-
-		  }       
 	
+	
+	//!\\ Test d'une animation ( merci open classroom
+
+
+
+
+private InterfaceUtilisateur pan = new InterfaceUtilisateur();
+
+	private int posY;
+
+	private int posX;
+
+
+
+public int getPosX() {
+  return posX;
 }
+
+public void setPosX(int posX) {
+  this.posX = posX;
+}
+
+public int getPosY() {
+  return posY;
+}
+
+public void setPosY(int posY) {
+  this.posY = posY;
+}
+
+
+private void go(){  
+
+  //Les coordonnées de départ de notre rond
+
+  int x = pan.getPosX(), y = pan.getPosY();
+
+  //Le booléen pour savoir si l'on recule ou non sur l'axe x
+
+  boolean backX = false;
+
+  //Le booléen pour savoir si l'on recule ou non sur l'axe y
+
+  boolean backY = false;
+
+  
+
+  //Dans cet exemple, j'utilise une boucle while
+
+  //Vous verrez qu'elle fonctionne très bien
+
+  while(true){
+
+    //Si la coordonnée x est inférieure à 1, on avance
+
+    if(x < 1)backX = false;
+
+    //Si la coordonnée x est supérieure à la taille du Panneau moins la taille du rond, on recule
+
+    if(x > pan.getWidth()-50)backX = true;
+
+    //Idem pour l'axe y
+
+    if(y < 1)backY = false;
+
+    if(y > pan.getHeight()-50)backY = true;
+
+    
+
+    //Si on avance, on incrémente la coordonnée
+
+    if(!backX)
+
+      pan.setPosX(++x);
+
+    //Sinon, on décrémente
+
+    else
+
+      pan.setPosX(--x);
+
+    //Idem pour l'axe Y
+
+    if(!backY)
+
+      pan.setPosY(++y);
+
+    else
+
+      pan.setPosY(--y);
+
+      
+
+    //On redessine notre Panneau
+
+    pan.repaint();
+
+    //Comme on dit : la pause s'impose ! Ici, trois millièmes de seconde
+
+    try {
+
+      Thread.sleep(3);
+
+    } catch (InterruptedException e) {
+
+      e.printStackTrace();
+
+    }
+
+  }    
+
+}  
+
+}
+	  
+	  
+	  
+	  
+	  
+	
+
