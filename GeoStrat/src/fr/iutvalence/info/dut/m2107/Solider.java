15cfa3dd30d1f80dca926f7571c724c5c3b8c3ac
aliotm <aliotm@PC-B123-14.iut-valence.fr>
2016-05-26 08:36:34 +0200

    bug

--- GeoStrat/src/fr/iutvalence/info/dut/m2107/Solider.java
+++ GeoStrat/src/fr/iutvalence/info/dut/m2107/Solider.java
@@ -2,5 +2,15 @@ package fr.iutvalence.info.dut.m2107;
 
 public class Solider extends Unite
 {
-
+	public void MySoldier()
+	{
+		super.Nom = "Fantassin";
+		super.numeroUnite = 0;
+		super.prix = 0;
+		super.statDepla = 5;
+		super.statAtt = 5;
+		super.statDef = 2;
+		super.EtatAttente = true;
+		super.pDV = 10;
+	}
 }
