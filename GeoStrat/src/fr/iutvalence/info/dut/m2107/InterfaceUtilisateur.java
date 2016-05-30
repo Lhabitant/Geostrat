3d55230023fddeef00778d3193cb6dbd07917dea
Loïc <loic.lecomte@iut-valence.fr>
2016-05-30 14:39:06 +0200

    test d'une grille

--- GeoStrat/src/fr/iutvalence/info/dut/m2107/InterfaceUtilisateur.java
+++ GeoStrat/src/fr/iutvalence/info/dut/m2107/InterfaceUtilisateur.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import java.awt.Color; 
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,15 +28,27 @@ public class InterfaceUtilisateur extends JFrame implements ActionListener {
 //*******************************************************************************************
 //!\\ User Interface in construction //!\\
 //*******************************************************************************************	
+	
+
+
+	
+	
 	private InterfaceUtilisateurEnJeu pan = new InterfaceUtilisateurEnJeu();
-	private JPanel container = new JPanel();
+	//private JPanel container = new JPanel();
+	private JPanel but = new JPanel();
+	private JPanel test = new JPanel();
 	// we create a new JLabel
     JLabel label = new JLabel("JLabel");
     // create a new button with the class Bouton
     JButton boutonEnd = new JButton(new EndTurnAction("Fin de tour"));
     JButton bouton1 = new JButton(new Action1("bouton1"));
     
-	private final int RESX = 1200;
+    JButton boutonA = new JButton("A");
+    JButton boutonB = new JButton("B");
+    JButton boutonC = new JButton("C");
+    JButton boutonD = new JButton("D");
+	
+    private final int RESX = 1200;
 	private final int RESY = 700;
 	
 	private final int LENDBUT = 150;
@@ -64,53 +77,34 @@ public class InterfaceUtilisateur extends JFrame implements ActionListener {
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
 	    //we can see the window
 	    this.setVisible(true);
-	    
 	    this.setLocationRelativeTo(null);
-
-	    //add the button "fin de tour" on the screen
-	    pan.add(boutonEnd);
-	    //add the button "bouton1" on the screen 
-	    pan.add(bouton1);
-	    // permet de fixer les boutons
-	    pan.setLayout(null);
-	    container.setLayout(new BorderLayout());
 	    
-	    this.setContentPane(pan);
+	    but.setLayout(new GridLayout(2,2,3,3));
+	    but.add(boutonA);
+	    but.add(boutonB);
+	    but.add(boutonC);
+	    but.add(boutonD);
+	    //add the button "fin de tour" on the screen
+	    test.add(boutonEnd);
+	    //add the button "bouton1" on the screen 
+	    test.add(bouton1);
+	    // permet de fixer les boutons
+	    test.setLayout(null);
+	    setLayout(null);
+	    but.setBounds(100, 100, 500, 375);
+	   // container.setLayout(new BorderLayout());
+	    
+	    //display they different Jpanel
+	    this.setContentPane(test);
+	    this.setContentPane(but);
+	   
 	    this.setVisible(true);
+	    
 	    
 	    //postion of end turn buttom (here bouton) (x,y,longueur,largueur)
 	    boutonEnd.setBounds(ENDBUTX, ENDBUTY, LENDBUT,  lENDBUT);
 	    bouton1.setBounds(BUTX, BUTY, LENDBUT,  lENDBUT);
-	    // position of a Label
-	    container.add(label, BorderLayout.NORTH);
 	    //go();
-	    
-	    
-	    
-	    Object[][] data = {
-
-	    	      {"Cysboy", "28 ans", "1.80 m"},
-
-	    	      {"BZHHydde", "28 ans", "1.80 m"},
-
-	    	      {"IamBow", "24 ans", "1.90 m"},
-
-	    	      {"FunMan", "32 ans", "1.85 m"}
-
-	    	    };
-
-
-	    	    //Les titres des colonnes
-
-	    	    String  title[] = {"Pseudo", "Age", "Taille"};
-
-	    	    JTable tableau = new JTable(data, title);
-
-	    	    //Nous ajoutons notre tableau à notre contentPane dans un scroll
-
-	    	    //Sinon les titres des colonnes ne s'afficheront pas !
-
-	    	    this.getContentPane().add(new JScrollPane(tableau));
 	    
 	    
 	
